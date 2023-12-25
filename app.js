96785035ba157b8cba3a0f3b61cef8ff45f4a0fc
macdaegeon <chleorjs37@gmail.com>
2023-12-25 14:56:18 +0900

    socket 오픈

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000;
+const cors = require('cors')
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io')
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+})
 const mysql = require('mysql');
 if (process.env.NODE_ENV === "production") {
     require('dotenv').config({ path: "config/env/.env.production" });
@@ -17,10 +22,6 @@ else {
     require('dotenv').config({ path: "config/env/.env.development" });
 }
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -28,23 +29,31 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
 app.get("/users", (req, res) => {
-    //connection.connect();
     connection.query('SELECT * from Users', (error, rows, fields) => {
         if (error) throw error;
         console.log('User info is: ', rows);
         res.send(rows)
     });
+});
 
-    //connection.end();
 
-})
+io.on('connection', (socket) => {
+    socket.on("send", (req) => {
+        setTimeout(() => {
+            socket.emit("resend", {
+                data: '반사'
+            })
+        }, 3000);
+    });
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+server.listen(port, () => {
+    console.log(`Socket IO server Example app listening on port ${port}`)
 })
